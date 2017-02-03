98cc94d6f11a10876c1dd134a4003e6577e3289f
tk26 <me@tejaskumthekar.com>
2017-02-03 16:35:05 -0500

    Add compilable version of a DoublyLinkedList

--- doublyLinkedList.c
+++ doublyLinkedList.c
@@ -20,80 +20,68 @@ struct node{
     struct node *next;
 };
 
-struct node *head = NULL;
-//head = malloc(sizeof(struct node));
-//head->process_id = 110;
-//head->key = INT_MAX;
+struct node *head;
+struct node *tail;
 
-struct node *tail = NULL;
-
-//create tail
-struct node *tail = (struct node*) malloc(sizeof(struct node));
-tail->process_id = 111; //assumption
-tail->key = INT_MIN;
+void createHeadTail(){
+    head->key = INT_MAX;
+    head->process_id = 110; //assumption
+    tail->process_id = 111; //assumption
+    tail->key = INT_MIN;
+}
 
 //return next of head
-struct *node getFirst(){
-    return head.next;
+struct node getFirst(){
+    return *head->next;
 }
 
 int getFirstID(){
-    return queuetab[head.process_id].next.process_id;
+    return head->next->process_id;
 }
 
 //return prev of tail
-struct *node getLast(){
-    return tail.prev;
+struct node getLast(){
+    return *tail->prev;
 }
 
 int getLastID(){
-    return queuetab[tail.process_id].prev.process_id;
+    return tail->prev->process_id;
 }
 
 bool isEmpty(){
-    //struct node *first = getFirstID();
-    //return first.process_id >= NPROC;
-
     //get the first node's ID with getFirstID
     int first = getFirstID();
     return first >= NPROC;
 }
 
 bool nonEmpty(){
-    //struct node *first = getFirstID();
-    //return first.process_id < NPROC;
-
     //get the first node's ID with getFirstID
     int first = getFirstID();
     return first < NPROC;
 }
 
 int getFirstKey(){
-    //get the first node with getFirst
-    struct node *first = getFirst();
-    return first.key;
+    return head->next->key;
 }
 
 int getLastKey(){
-    //get the last node with getLast
-    struct node *last = getLast();
-    return last.key;
+    return tail->prev->key;
 }
 
 //FIFO manipulations
 
-struct *node enqueue(int processID){
-    struct node *prev = tail.prev;
+struct node enqueue(int processID){
+    struct node *prev = tail->prev;
 
     //insert before tail
     struct node *newproc = (struct node*) malloc(sizeof(struct node));
-    newproc.process_id = processID;
-    newproc.next = tail;
-    newproc.prev = prev;
-    prev.next = newproc;
-    tail.prev = newproc;
+    newproc->process_id = processID;
+    newproc->next = tail;
+    newproc->prev = prev;
+    prev->next = newproc;
+    tail->prev = newproc;
 
-    return newproc;
+    return *newproc;
 }
 
 int dequeue(){
@@ -103,9 +91,9 @@ int dequeue(){
         return EMPTY;
     }
     *target = getFirst();
-    pid = target.process_id;
-    target.prev = NULL;
-    target.next = NULL;
+    pid = target->process_id;
+    target->prev = NULL;
+    target->next = NULL;
     return pid;
 }
 
@@ -118,21 +106,25 @@ int insert(int processID, int key){
         return ERROR;
     }
 
-    curr = getFirst();
-    while(curr.key >= key){
-        curr = curr.next;
+    *curr = getFirst();
+    while(curr->key >= key){
+        curr = curr->next;
     }
 
     struct node *newproc = (struct node*) malloc(sizeof(struct node));
-    newproc.process_id = processID;
+    newproc->process_id = processID;
 
     //insert process
-    prev = curr.prev;
-    newproc.next = curr;
-    newproc.prev = prev;
-    newproc.key = key;
-    prev.next = newproc;
-    curr.prev = newproc;
+    prev = curr->prev;
+    newproc->next = curr;
+    newproc->prev = prev;
+    newproc->key = key;
+    prev->next = newproc;
+    curr->prev = newproc;
 
     return OK;
+}
+
+int main(){
+    return 0;
 }
