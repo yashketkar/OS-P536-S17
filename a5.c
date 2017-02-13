9756a00cd8a9cabf96660ebe6066fd88f8a09b16
Yash Sumant Ketkar <yketkar@indiana.edu>
2017-02-13 18:57:40 -0500

    Update rand_delay

--- a5.c
+++ a5.c
@@ -5,13 +5,12 @@
 void rand_delay(int uS_max)
 {
 	int random_number = rand() % (uS_max + 1);
-	printf("The random number is : %d \n",random_number);
+	printf("This program will exit in %d seconds.\n",random_number);
 	usleep(random_number);
 }
 
 int main()
 {
-	printf("This c program will exit in 1 seconds.\n");
 	rand_delay(100000000);
 	return 0;
 }
